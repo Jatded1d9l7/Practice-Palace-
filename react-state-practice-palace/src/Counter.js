3231d3b916305f6e6c12df3deb1b93d2c636c669
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-21 22:23:10 -0400

    initial commit

---
+++ react-state-practice-palace/src/Counter.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Counter: { this.state.count }</h2>
+      </div>
+    )
+  }
+}
+
+export default Counter
