54e97bf7598bcd6829dc6bfbc259e8747e0ebfd0
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-29 17:51:25 -0400

    update

---
+++ practice.js
@@ -0,0 +1,26 @@
+// string multiple words 
+//capitalizes all words in string
+// first character in each word
+
+//psuedeo code
+// create a function = firstCaps
+// pararameter string of words 
+// output= first character in string capitalized
+
+input "bob is awesome"
+expected output "Bob Is Awesome"
+
+describe ('firstCaps') => 
+test ("first character in string capitalized")
+expect(firstCaps).toEqual(Bob Is Awesome)
+
+
+const firstCaps = (str) => {
+  str.split("")
+}
+
+
+
+// look at each index 
+//map over zeroith index .map() 
+//capitalize 
